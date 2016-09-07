72ad758634dd1fa5022d49be1d59f02d49c8121c
Agung Yuliaji <agung.yuliaji@gmail.com>
2016-09-07 11:40:24 +0700

    fix env get

--- app/models/geoip/base.rb
+++ app/models/geoip/base.rb
@@ -4,6 +4,6 @@ module Geoip
     self.abstract_class = true
     
     # establish_connection(ActiveRecord::Base.configurations["geoip_#{::Rails.env}"])
-    establish_connection(YAML.load_file("#{::Rails.root}/config/geoip.yml")[::Rails.env])
+    establish_connection(YAML.load_file("#{::Rails.root}/config/geoip.yml")["geoip_#{::Rails.env}"])
   end
 end
